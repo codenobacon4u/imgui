74fc973c53b88befd5026145446adf7893c85ea1
codenobacon4u <kralcmit@gmail.com>
2024-01-01 03:13:03 -0500

    Fix static runtime setting cohesion

--- premake5.lua
+++ premake5.lua
@@ -23,7 +23,7 @@ project "ImGui"
     filter "system:windows"
         systemversion "latest"
         cppdialect "C++17"
-        staticruntime "On"
+        staticruntime "off"
 
         
     filter "configurations:Debug"
